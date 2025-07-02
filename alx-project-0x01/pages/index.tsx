6b81649a0d62bf37cc661be8047b198131332e9c
Esau Jesse <jesseesau13@gmail.com>
2025-07-02 09:41:44 +0100

    Basic setup

---
+++ alx-project-0x01/pages/index.tsx
@@ -0,0 +1,11 @@
+const Home: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-7xl font-thin">Welcome Page</h1>
+      <p className="text-6xl text-red-500">Test</p>
+      <p>Test 2</p>
+    </div>
+  )
+}
+
+export default Home;
---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,9 @@
+const UsersPage: React.FC = () => {
+    return (
+        <div className="p-8">
+            <h1 className="text-3xl font-bold mb-4">All Users</h1>
+        </div>
+    );
+};
+
+export default UsersPage;
