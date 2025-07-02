ecdce49432028fe602c8bf76fd75c6be8028daa3
Esau Jesse <jesseesau13@gmail.com>
2025-07-02 09:55:20 +0100

    Update basic structure

---
+++ alx-project-0x01/pages/posts/index.tsx
@@ -0,0 +1,9 @@
+const PostsPage: React.FC = () => {
+    return (
+        <div className="p-8">
+            <h1 className="text-3xl font-bold mb-4">All Posts</h1>
+        </div>
+    );
+};
+
+export default PostsPage;
